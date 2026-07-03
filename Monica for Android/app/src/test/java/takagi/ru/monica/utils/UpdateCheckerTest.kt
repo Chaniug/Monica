58364c16c4311e1e62daa98ad008b9369e705d8c
JoyinJoester <lichaoran8@gmail.com>
2026-07-04 00:04:19 +0800

    Bump Android version to 1.0.292

--- Monica for Android/app/src/test/java/takagi/ru/monica/utils/UpdateCheckerTest.kt
+++ Monica for Android/app/src/test/java/takagi/ru/monica/utils/UpdateCheckerTest.kt
@@ -7,22 +7,22 @@ import org.junit.Test
 class UpdateCheckerTest {
     @Test
     fun compareVersionTags_detectsNewerRelease() {
-        assertTrue(UpdateChecker.compareVersionTags("v1.0.291", "1.0.291") > 0)
+        assertTrue(UpdateChecker.compareVersionTags("v1.0.293", "1.0.292") > 0)
     }
 
     @Test
     fun compareVersionTags_treatsMatchingPrefixAsSameVersion() {
-        assertEquals(0, UpdateChecker.compareVersionTags("v1.0.291", "1.0.291-preview"))
+        assertEquals(0, UpdateChecker.compareVersionTags("v1.0.292", "1.0.292-preview"))
     }
 
     @Test
     fun compareVersionTags_ignoresReleaseLetterSuffix() {
-        assertEquals(0, UpdateChecker.compareVersionTags("V1.0.291c", "1.0.291"))
-        assertEquals(0, UpdateChecker.compareVersionTags("V1.0.291c", "V1.0.291b"))
+        assertEquals(0, UpdateChecker.compareVersionTags("V1.0.292c", "1.0.292"))
+        assertEquals(0, UpdateChecker.compareVersionTags("V1.0.292c", "V1.0.292b"))
     }
 
     @Test
     fun compareVersionTags_detectsOlderRelease() {
-        assertTrue(UpdateChecker.compareVersionTags("1.0.287", "1.0.291") < 0)
+        assertTrue(UpdateChecker.compareVersionTags("1.0.287", "1.0.292") < 0)
     }
 }
